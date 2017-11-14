2d3a633df287ecdb87e358321c9de88afc426e9b
Yexing <yexingok@gmail.com>
2017-11-14 15:02:19 +0800

    add old screen configuration

--- install.sh
+++ install.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env bash
 
+case `uname` in 
+    Darwin)
+        echo 'MacOS'
+        brew install zsh macvim stow tig ctags wget 
+        ;;
+    Linux)
+        echo 'Make sure we have required packages on our Linux'
+        sudo yum install -y git tig vim zsh ctags screen tmux 
+        ;;
+esac
+
+if [ ! -d ${HOME}/.vim/bundle/Vundle.vim ] ; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
+fi
+
 for d in `ls -d */`;
 do 
 	( stow $d )
 done
+
+echo ""
+echo "Done! open vim and run plugin install:"
+echo ":PluginInstall"
+
