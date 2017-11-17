05af199539bd117ce46ec2df88d138940270fe32
Yexing <yexingok@gmail.com>
2017-11-17 15:26:12 +0800

    Check oh-my-zsh path

--- install.sh
+++ install.sh
@@ -3,13 +3,17 @@
 case `uname` in 
     Darwin)
         echo 'MacOS'
-        brew install zsh macvim stow tig ctags wget zsh-completions
+        brew install zsh macvim stow tig ctags zsh-completions curl wget
         ;;
     Linux)
         echo 'Make sure we have required packages on our Linux'
-        sudo yum install -y git tig vim zsh ctags screen tmux stow
+        sudo yum install -y git tig vim zsh ctags screen tmux stow curl wget
         ;;
 esac
+
+if [ ! -d ${HOME}/.oh-my-zsh ] ; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
 
 if [ ! -d ${HOME}/.vim/bundle/Vundle.vim ] ; then
     git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
